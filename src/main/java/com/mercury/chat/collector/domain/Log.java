765fef98230076d919a8ee744e818f49afcce488
creaton60 <lovmistake@gmail.com>
2021-02-19 21:21:25 +0900

    [update] log attribute

--- src/main/java/com/mercury/chat/collector/domain/Log.java
+++ src/main/java/com/mercury/chat/collector/domain/Log.java
@@ -1,4 +1,16 @@
 package com.mercury.chat.collector.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Builder
 public class Log {
+    private String id;
+    private String message;
+    private LocalDateTime created;
 }
