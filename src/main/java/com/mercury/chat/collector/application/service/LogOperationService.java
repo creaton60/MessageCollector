29159130ea7f551b566e1edc0e863c991c558333
creaton60 <lovmistake@gmail.com>
2021-02-26 21:29:30 +0900

    [update] insert method

--- src/main/java/com/mercury/chat/collector/application/service/LogOperationService.java
+++ src/main/java/com/mercury/chat/collector/application/service/LogOperationService.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class LogOperationService implements CreateLogUseCase {
+    @Override
+    public void insertCreateLog(CreateLogQuery query) {
+
+    }
 }
