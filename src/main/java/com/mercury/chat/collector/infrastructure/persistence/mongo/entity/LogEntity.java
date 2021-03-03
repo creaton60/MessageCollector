f7247fbdc472446472ef90e8c08a60295a18f0b8
creaton60 <lovmistake@gmail.com>
2021-03-03 21:34:38 +0900

    [update] entity attribute

--- src/main/java/com/mercury/chat/collector/infrastructure/persistence/mongo/entity/LogEntity.java
+++ src/main/java/com/mercury/chat/collector/infrastructure/persistence/mongo/entity/LogEntity.java
@@ -1,4 +1,21 @@
 package com.mercury.chat.collector.infrastructure.persistence.mongo.entity;
 
-public class LogEntity {
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class LogEntity implements Serializable {
+    private String id;
+
+    private String message;
+
+    private LocalDateTime created;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime lastModified;
 }
